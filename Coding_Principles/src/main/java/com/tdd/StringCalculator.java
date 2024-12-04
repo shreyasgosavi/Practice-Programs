5d16733f0cbf1901edc1ab0a8c3f6d2dedeaba18
shreyasgosavi <shreyasgosavi2016@gmail.com>
2024-12-04 16:16:09 +0530

    Goal mentioned in the main-class & first test-case written. 1. Expectation to output 0 when string is empty/contains white spaces 2. Expectation to output same number when entered as input
    Test-case failed

--- Coding_Principles/src/main/java/com/tdd/StringCalculator.java
+++ Coding_Principles/src/main/java/com/tdd/StringCalculator.java
@@ -1,4 +1,7 @@
 package com.tdd;
 
 public class StringCalculator {
+
+    // Goal : To take input as a string which will tell what operation to perform with subsequent  numbers separated by delimiter mentioned in the input itself.
+
 }
--- Coding_Principles/src/test/java/com/tdd/StringCalulatorTest.java
+++ Coding_Principles/src/test/java/com/tdd/StringCalulatorTest.java
@@ -1,7 +1,18 @@
 package com.tdd;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+class StringCalulatorTest {
 
+    StringCalculator sc = new StringCalculator();
+    @Test
+    void getResultTest(String numbers){
 
-public class StringCalulatorTest {
+        //Test for empty-string
+        Assertions.assertEquals(0, sc.getResult(""));
+        //Test for single-digit value
+        Assertions.assertEquals(1,sc.getResult("1"));
+
+    }
 
 
 }
