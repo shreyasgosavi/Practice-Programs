b1159c6a11416791503ab4ffb15204137da3184b
shreyasgosavi <shreyasgosavi2016@gmail.com>
2024-12-04 18:56:03 +0530

    Goal mentioned in the main-class & first test-case written.
    6. Expectation : If there are characters other than numbers ignore/skip it & consider subsequent numbers | Optional -- print values that were not considered
    
    Test-case : Passed !!

--- Coding_Principles/src/main/java/com/tdd/StringCalculator.java
+++ Coding_Principles/src/main/java/com/tdd/StringCalculator.java
@@ -1,19 +1,31 @@
 package com.tdd;
 
+import java.util.ArrayList;
+
 public class StringCalculator {
 
     // Goal : To take input as a string which will tell what operation to perform with subsequent  numbers separated by delimiter mentioned in the input itself.
 
     public int getResult(String input){
 
-        if(input.isEmpty()){
+        if(input == null || input.isEmpty()){
             return 0;
         }
+
         String[] numbers = input.split(",+",0);
+        ArrayList<String> ignoredValue = new ArrayList<>();
+
         int sum = 0;
         for(String s : numbers){
-            sum += Integer.valueOf(s.trim());
+
+            //Adding exceptional-handling to take care of values other than integers
+            try {
+                sum += Integer.valueOf(s.trim());
+            }catch(Exception e){
+                ignoredValue.add(s);
+            }
         }
+
         return sum;
     }
 
